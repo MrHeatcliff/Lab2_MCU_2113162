cb5dbfefbb5236d2ec24ed6363fc70e1562e986f
heathcliff <dat.tranheathcliff@hcmut.edu.vn>
2023-10-17 16:15:11 +0700

    delete file

---
+++ Lab2_ex5/Core/Src/updateClockBuffer.h
@@ -0,0 +1,19 @@
+/*
+ * updateClockBuffer.h
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: Heathcliff
+ */
+
+#ifndef SRC_UPDATECLOCKBUFFER_H_
+#define SRC_UPDATECLOCKBUFFER_H_
+
+//void updateClockBuffer()
+//{
+//	led_buffer[0] = hour/10;
+//	led_buffer[1] = hour%10;
+//	led_buffer[2] = minute/10;
+//	led_buffer[3] = minute%10;
+//}
+
+#endif /* SRC_UPDATECLOCKBUFFER_H_ */
